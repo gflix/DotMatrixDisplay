f83c369880ef03b495167aafead6cf447568f5e2
Felix Mellmann <felix.mellmann@googlemail.com>
2018-03-31 18:54:44 +0200

    Try to access the ImageView.

--- app/src/main/java/de/benfm/dotmatrixdisplay/FullscreenActivity.java
+++ app/src/main/java/de/benfm/dotmatrixdisplay/FullscreenActivity.java
@@ -3,9 +3,9 @@ package de.benfm.dotmatrixdisplay;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.util.Log;
+import android.widget.ImageView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,6 +15,7 @@ public class FullscreenActivity extends AppCompatActivity
 {
     private static final String TAG = "FullscreenActivity";
     private View mContentView;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,6 +26,7 @@ public class FullscreenActivity extends AppCompatActivity
         setContentView(R.layout.activity_fullscreen);
 
         mContentView = findViewById(R.id.fullscreen_content);
+        mImageView = findViewById(R.id.image_view);
 
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,8 +39,10 @@ public class FullscreenActivity extends AppCompatActivity
             @Override
             public void run() {
                 Log.i(TAG, "post()");
-                Log.i(TAG, Integer.toString(mContentView.getWidth()));
-                Log.i(TAG, Integer.toString(mContentView.getHeight()));
+                Log.i(TAG, "mContentView.width=" + Integer.toString(mContentView.getWidth()));
+                Log.i(TAG, "mContentView.height=" + Integer.toString(mContentView.getHeight()));
+
+                loadAnotherImage();
             }
         });
 
@@ -70,5 +74,11 @@ public class FullscreenActivity extends AppCompatActivity
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    protected void loadAnotherImage()
+    {
+        Log.i(TAG, "mImageView.width=" + Integer.toString(mImageView.getWidth()));
+        Log.i(TAG, "mImageView.height=" + Integer.toString(mImageView.getHeight()));
     }
 }
