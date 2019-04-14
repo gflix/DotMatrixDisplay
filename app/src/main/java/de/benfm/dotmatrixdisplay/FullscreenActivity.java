80ae8c5b1913a111487af90c699a2c4f71a21fba
Felix Mellmann <felix.mellmann@gmail.com>
2019-04-14 15:55:40 +0200

    Attempt to draw a custom texture to a rectangle.

--- app/src/main/java/de/benfm/dotmatrixdisplay/FullscreenActivity.java
+++ app/src/main/java/de/benfm/dotmatrixdisplay/FullscreenActivity.java
@@ -1,5 +1,7 @@
 package de.benfm.dotmatrixdisplay;
 
+import android.graphics.BitmapFactory;
+import android.opengl.GLU;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,17 +9,24 @@ import android.util.Log;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 import android.os.Bundle;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 /**
  * A vertex shaded cube.
  */
+/*
 class Cube {
     public Cube() {
         int one = 0x10000;
@@ -69,11 +78,11 @@ class Cube {
     private IntBuffer mColorBuffer;
     private ByteBuffer mIndexBuffer;
 }
-
+*/
 /**
  * Render a pair of tumbling cubes.
  */
-
+/*
 class CubeRenderer implements GLSurfaceView.Renderer {
     public CubeRenderer(boolean useTranslucentBackground) {
         mTranslucentBackground = useTranslucentBackground;
@@ -81,17 +90,17 @@ class CubeRenderer implements GLSurfaceView.Renderer {
     }
 
     public void onDrawFrame(GL10 gl) {
-        /*
+        *//*
          * Usually, the first thing one might want to do is to clear the screen.
          * The most efficient way of doing this is to use glClear().
          */
 
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+/*        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-        /*
+        *//*
          * Now we're ready to draw some 3D objects
          */
-
+/*
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
         gl.glTranslatef(0, 0, -3.0f);
@@ -114,31 +123,31 @@ class CubeRenderer implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         gl.glViewport(0, 0, width, height);
 
-        /*
+        *//*
          * Set our projection matrix. This doesn't have to be done each time we
          * draw, but usually a new projection needs to be set when the viewport
          * is resized.
          */
 
-        float ratio = (float) width / height;
+/*        float ratio = (float) width / height;
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
         gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
     }
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        /*
+        *//*
          * By default, OpenGL enables features that improve quality but reduce
          * performance. One might want to tweak that especially on software
          * renderer.
          */
-        gl.glDisable(GL10.GL_DITHER);
+/*        gl.glDisable(GL10.GL_DITHER);
 
-        /*
+        *//*
          * Some one-time OpenGL initialization can be made here probably based
          * on features of this particular context
          */
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+/*        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
 
         if (mTranslucentBackground) {
             gl.glClearColor(0, 0, 0, 0);
@@ -154,6 +163,175 @@ class CubeRenderer implements GLSurfaceView.Renderer {
     private Cube mCube;
     private float mAngle;
 }
+*/
+
+class Square {
+    private FloatBuffer vertexBuffer;
+    private float vertices[] = {
+            -1.0f, -1.0f, 0.0f,
+            -1.0f,  1.0f, 0.0f,
+             1.0f, -1.0f, 0.0f,
+             1.0f,  1.0f, 0.0f
+    };
+
+    private FloatBuffer textureBuffer;
+    private float texture[] = {
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f
+    };
+
+    public Square(Resources resources)
+    {
+        this.resources = resources;
+
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        vertexBuffer = byteBuffer.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
+
+        byteBuffer = ByteBuffer.allocateDirect(texture.length * 4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        textureBuffer = byteBuffer.asFloatBuffer();
+        textureBuffer.put(texture);
+        textureBuffer.position(0);
+    }
+
+    public void draw(GL10 gl)
+    {
+        if (textureId <= 0)
+        {
+            reloadAssets(gl);
+        }
+
+        /*
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glColor4f(0.0f, 1.0f, 0.75f, 1.0f);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        */
+
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureId);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+        gl.glFrontFace(GL10.GL_CW);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+
+        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+    }
+
+    public void reloadAssets(GL10 gl)
+    {
+        Log.i(TAG, "reloadAssets()");
+        int[] textureIds = new int[1];
+        gl.glGenTextures(1, textureIds, 0);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureIds[0]);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(resources, R.mipmap.led_on);
+        Log.i(TAG, "reloadAssets(" + bitmap.getWidth() + ", " + bitmap.getHeight() + ")");
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+        Log.i(TAG, "reloadAssets(" + GLUtils.getEGLErrorString(gl.glGetError()) + ")");
+        bitmap.recycle();
+
+        textureId = textureIds[0];
+        Log.i(TAG, "reloadAssets(" + textureId + ")");
+    }
+
+    private static final String TAG = "Square";
+    private Resources resources = null;
+    private int textureId = -1;
+}
+
+class DotMatrixRenderer implements GLSurfaceView.Renderer {
+    public DotMatrixRenderer(Resources resources) {
+        this.square = new Square(resources);
+    }
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        Log.i(TAG, "onSurfaceCreated()");
+        square.reloadAssets(gl);
+/*        gl.glDisable(GL10.GL_DITHER);
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+        gl.glClearColor(0, 0, 0, 0);
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+
+        int[] textureIds = new int[1];
+        gl.glGenTextures(1, textureIds, 0);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, textureIds[0]);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
+        gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(resources, R.mipmap.led_on);
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+        bitmap.recycle();
+
+        textureId = textureIds[0];*/
+    }
+
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        Log.i(TAG, "onSurfaceChanged(" + width + ", " + height + ")");
+        if (height == 0)
+        {
+            height = 1;
+        }
+
+        gl.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+//        gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+        GLU.gluPerspective(gl, 45.0f, ratio, 0.1f, 100.0f);
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    public void onDrawFrame(GL10 gl) {
+        Log.i(TAG, "onDrawFrame()");
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, 0.0f, -5.0f);
+
+        square.draw(gl);
+/*
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0, 0, -3.0f);
+        gl.glRotatef(mAngle, 0, 1, 0);
+        gl.glRotatef(mAngle * 0.25f, 1, 0, 0);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+        mCube.draw(gl);
+
+//        gl.glRotatef(mAngle * 2.0f, 0, 1, 1);
+//        gl.glTranslatef(0.5f, 0.5f, 0.5f);
+
+//        mCube.draw(gl);
+
+        mAngle += 0.5f;
+        */
+    }
+
+    private static final String TAG = "DotMatrixRenderer";
+    private Square square;
+}
 
 /**
  * Wrapper activity demonstrating the use of {@link GLSurfaceView}, a view that
@@ -167,9 +345,9 @@ public class FullscreenActivity extends AppCompatActivity {
         // Create our Preview view and set it as the content of our
         // Activity
         mGLSurfaceView = new GLSurfaceView(this);
-        mGLSurfaceView.setRenderer(new CubeRenderer(true));
-        setContentView(mGLSurfaceView);
+        mGLSurfaceView.setRenderer(new DotMatrixRenderer(getResources()));
         mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        setContentView(mGLSurfaceView);
 
         mGLSurfaceView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -200,7 +378,6 @@ public class FullscreenActivity extends AppCompatActivity {
 
     protected void restoreFullscreen()
     {
-        Log.i(TAG, "restoreFullscreen");
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
         {
