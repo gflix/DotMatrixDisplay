59b5e7aa5e4e31e8de6b5d09b6996971d8c9bc67
Felix Mellmann <felix.mellmann@gmail.com>
2019-04-14 13:24:18 +0200

    Use GLSurfaceView as shown in http://www.java2s.com/Code/Android/2D-Graphics/WrapperactivitydemonstratingtheuseofGLSurfaceViewaviewthatusesOpenGLdrawingintoadedicatedsurface.htm and draw a cube. Dot matrix display is currently not functional.

--- app/src/main/java/de/benfm/dotmatrixdisplay/FullscreenActivity.java
+++ app/src/main/java/de/benfm/dotmatrixdisplay/FullscreenActivity.java
@@ -1,14 +1,226 @@
 package de.benfm.dotmatrixdisplay;
 
-import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
 
-import java.util.Random;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+/**
+ * A vertex shaded cube.
+ */
+class Cube {
+    public Cube() {
+        int one = 0x10000;
+        int vertices[] = { -one, -one, -one, one, -one, -one, one, one, -one,
+                -one, one, -one, -one, -one, one, one, -one, one, one, one,
+                one, -one, one, one, };
+
+        int colors[] = { 0, 0, 0, one, one, 0, 0, one, one, one, 0, one, 0,
+                one, 0, one, 0, 0, one, one, one, 0, one, one, one, one, one,
+                one, 0, one, one, one, };
+
+        byte indices[] = { 0, 4, 5, 0, 5, 1, 1, 5, 6, 1, 6, 2, 2, 6, 7, 2, 7,
+                3, 3, 7, 4, 3, 4, 0, 4, 7, 6, 4, 6, 5, 3, 0, 1, 3, 1, 2 };
+
+        // Buffers to be passed to gl*Pointer() functions
+        // must be direct, i.e., they must be placed on the
+        // native heap where the garbage collector cannot
+        // move them.
+        //
+        // Buffers with multi-byte datatypes (e.g., short, int, float)
+        // must have their byte order set to native order
+
+        ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+        vbb.order(ByteOrder.nativeOrder());
+        mVertexBuffer = vbb.asIntBuffer();
+        mVertexBuffer.put(vertices);
+        mVertexBuffer.position(0);
+
+        ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+        cbb.order(ByteOrder.nativeOrder());
+        mColorBuffer = cbb.asIntBuffer();
+        mColorBuffer.put(colors);
+        mColorBuffer.position(0);
+
+        mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+        mIndexBuffer.put(indices);
+        mIndexBuffer.position(0);
+    }
+
+    public void draw(GL10 gl) {
+        gl.glFrontFace(GL10.GL_CW);
+        gl.glVertexPointer(3, GL10.GL_FIXED, 0, mVertexBuffer);
+        gl.glColorPointer(4, GL10.GL_FIXED, 0, mColorBuffer);
+        gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE,
+                mIndexBuffer);
+    }
+
+    private IntBuffer mVertexBuffer;
+    private IntBuffer mColorBuffer;
+    private ByteBuffer mIndexBuffer;
+}
+
+/**
+ * Render a pair of tumbling cubes.
+ */
+
+class CubeRenderer implements GLSurfaceView.Renderer {
+    public CubeRenderer(boolean useTranslucentBackground) {
+        mTranslucentBackground = useTranslucentBackground;
+        mCube = new Cube();
+    }
+
+    public void onDrawFrame(GL10 gl) {
+        /*
+         * Usually, the first thing one might want to do is to clear the screen.
+         * The most efficient way of doing this is to use glClear().
+         */
+
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+        /*
+         * Now we're ready to draw some 3D objects
+         */
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0, 0, -3.0f);
+        gl.glRotatef(mAngle, 0, 1, 0);
+        gl.glRotatef(mAngle * 0.25f, 1, 0, 0);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
+        mCube.draw(gl);
+
+//        gl.glRotatef(mAngle * 2.0f, 0, 1, 1);
+//        gl.glTranslatef(0.5f, 0.5f, 0.5f);
+
+//        mCube.draw(gl);
+
+        mAngle += 0.5f;
+    }
+
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        gl.glViewport(0, 0, width, height);
+
+        /*
+         * Set our projection matrix. This doesn't have to be done each time we
+         * draw, but usually a new projection needs to be set when the viewport
+         * is resized.
+         */
+
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+    }
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        /*
+         * By default, OpenGL enables features that improve quality but reduce
+         * performance. One might want to tweak that especially on software
+         * renderer.
+         */
+        gl.glDisable(GL10.GL_DITHER);
+
+        /*
+         * Some one-time OpenGL initialization can be made here probably based
+         * on features of this particular context
+         */
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+
+        if (mTranslucentBackground) {
+            gl.glClearColor(0, 0, 0, 0);
+        } else {
+            gl.glClearColor(1, 1, 1, 1);
+        }
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+    }
+
+    private boolean mTranslucentBackground;
+    private Cube mCube;
+    private float mAngle;
+}
+
+/**
+ * Wrapper activity demonstrating the use of {@link GLSurfaceView}, a view that
+ * uses OpenGL drawing into a dedicated surface.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create our Preview view and set it as the content of our
+        // Activity
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setRenderer(new CubeRenderer(true));
+        setContentView(mGLSurfaceView);
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        mGLSurfaceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restoreFullscreen();
+                mGLSurfaceView.requestRender();
+            }
+        });
+
+        restoreFullscreen();
+    }
+
+    @Override
+    protected void onResume() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        // Ideally a game should implement onResume() and onPause()
+        // to take appropriate action when the activity looses focus
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+    protected void restoreFullscreen()
+    {
+        Log.i(TAG, "restoreFullscreen");
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.hide();
+        }
+
+        mGLSurfaceView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private static final String TAG = "FullscreenActivity";
+    private GLSurfaceView mGLSurfaceView;
+}
+
+/*
 public class FullscreenActivity extends AppCompatActivity
 {
     private static final String TAG = "FullscreenActivity";
@@ -75,3 +287,4 @@ public class FullscreenActivity extends AppCompatActivity
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
+*/
