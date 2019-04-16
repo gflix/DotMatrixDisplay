975106aa7233ffede0d994377cc86d8a47fc6ddf
Felix Mellmann <flix@benfm.de>
2019-04-16 18:38:40 +0000

    Toggle between fullscreen mode and visible action bar when tapping on the screen.

--- app/src/main/java/de/benfm/dotmatrixdisplay/MainActivity.java
+++ app/src/main/java/de/benfm/dotmatrixdisplay/MainActivity.java
@@ -28,20 +28,17 @@ public class MainActivity extends AppCompatActivity {
         glSurfaceView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                restoreFullscreen();
+                restoreFullscreen(false);
             }
         });
-
-        restoreFullscreen();
     }
 
     @Override
     protected void onStart()
     {
         super.onStart();
-//        Log.i(TAG, "onStart()");
 
-        restoreFullscreen();
+        restoreFullscreen(true);
         dotMatrixHandler.run();
     }
 
@@ -49,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop()
     {
         super.onStop();
-//        Log.i(TAG, "onStop()");
 
         handler.removeCallbacks(dotMatrixHandler);
     }
@@ -66,21 +62,39 @@ public class MainActivity extends AppCompatActivity {
         glSurfaceView.onPause();
     }
 
-    protected void restoreFullscreen()
+    protected void restoreFullscreen(boolean forceHide)
     {
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null)
+        int systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE |
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+        if (forceHide)
         {
-            actionBar.hide();
+            toogleFullscreen = true;
         }
 
-        glSurfaceView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LOW_PROFILE |
-                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        if (toogleFullscreen)
+        {
+            if (actionBar != null)
+            {
+                actionBar.hide();
+            }
+
+            systemUiVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        }
+        else
+        {
+            if (actionBar != null)
+            {
+                actionBar.show();
+            }
+        }
+        glSurfaceView.setSystemUiVisibility(systemUiVisibility);
+        toogleFullscreen = !toogleFullscreen;
     }
 
     private static final String TAG = "MainActivity";
@@ -88,4 +102,6 @@ public class MainActivity extends AppCompatActivity {
     private DotMatrix dotMatrix;
     private DotMatrixHandler dotMatrixHandler;
     private Handler handler;
+
+    private boolean toogleFullscreen = false;
 }
