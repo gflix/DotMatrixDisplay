3eb58200508e0435d80e2fba5cdc9dc102687a37
Felix Mellmann <felix.mellmann@gmail.com>
2019-04-19 14:01:33 +0200

    Allow entering the OpenWeather informations location ID and API key. The information are stored automagically.

--- app/src/main/java/de/benfm/dotmatrixdisplay/SettingsActivity.java
+++ app/src/main/java/de/benfm/dotmatrixdisplay/SettingsActivity.java
@@ -164,7 +164,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("open_weather_location_id"));
+            bindPreferenceSummaryToValue(findPreference("open_weather_api_ke"));
         }
 
         @Override
